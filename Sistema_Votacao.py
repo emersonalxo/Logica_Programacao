d0095db53f9ee18eb8f80001b58cd8503b2f6bc0
emersonalxo <e.aleixo@aluno.senai.br>
2026-05-04 21:56:56 -0300

    Add files via upload

---
+++ Sistema_Votacao.py
@@ -0,0 +1,6 @@
+idade = int(input("Digite sua idade: "))
+if idade == 16 or idade == 17 or idade > 70:
+    print("Seu Voto e opicional")
+else:
+    print("Seu voto é obrigatorio, ou você ainda não pode votar")
+    
