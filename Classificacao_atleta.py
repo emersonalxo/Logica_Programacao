efeee5f95a0da1f2046d1f71003da05e355afe1f
emersonalxo <e.aleixo@aluno.senai.br>
2026-05-04 22:14:32 -0300

    Add files via upload

---
+++ Classificacao_atleta.py
@@ -0,0 +1,9 @@
+idade = int(input("Digite sua idade:"))
+if idade >= 5 and idade <=10: 
+    print("Categoria Infantil")
+elif idade >= 11 and idade <=17:
+    print("Categoria: Juvenil")
+elif idade >= 18:
+    print("Categoria: Adulto")
+else:
+    print("Muito Jovem para Competir")
